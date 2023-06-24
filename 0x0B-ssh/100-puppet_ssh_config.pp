5ecf55301ddc39f7fabe8c18d574fbb7dc459c38
alx <atundeg@gmail.com>
2023-06-24 20:32:42 +0100

    use file line

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,22 +1,12 @@
-# make changes to configuration file
-package { 'augeas-tools':
-  ensure => installed,
+# make changes to configuration file, use private key
+include stdlib
+
+file_line { 'Declare identity file':
+  path  =>  '/etc/ssh/ssh_config',
+  line  =>  'IdentityFile ~/.ssh/holberton',
 }
 
-augeas { 'Declare identity file':
-  lens    => 'Ssh.lns',
-  incl    => '/etc/ssh/ssh_config',
-  changes => [
-    'set Host/IdentityFile[.="~/.ssh/school"] "~/.ssh/school"',
-  ],
-  require => Package['augeas-tools'],
-}
-
-augeas { 'Turn off passwd auth':
-  lens    => 'Ssh.lns',
-  incl    => '/etc/ssh/ssh_config',
-  changes => [
-    'set Host/PasswordAuthentication "no"',
-  ],
-  require => Package['augeas-tools'],
+file_line { 'Turn off Password Auth':
+  path  =>  '/etc/ssh/ssh_config',
+  line  =>  'PasswordAuthentication no',
 }
