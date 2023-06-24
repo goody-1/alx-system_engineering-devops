d4ff8b7f3ceeb93d1ef1229228fda29db202ab16
alx <atundeg@gmail.com>
2023-06-24 15:12:34 +0100

    add manifest to create file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# This manifest creates a file in '/tmp/'
+file { 'school':
+    ensure  => file,
+    path    => '/tmp/school',
+    content => 'I love Puppet',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744'
+}
