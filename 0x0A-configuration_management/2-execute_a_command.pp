9ee52110abbf9af7b499bdd72335e1ca8f5fc4cd
alx <atundeg@gmail.com>
2023-06-24 16:08:55 +0100

    set refreshonly value to default

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,5 @@
-# Kill a process 'killmenowing' with pkill
+# Kill a process 'killmenow' with pkill
 exec { 'killmenow_process':
-  command     => 'pkill killmenow',
-  path        => '/usr/bin:/bin',
-  onlyif      => 'pgrep killmenow',
-  refreshonly => true,
+  command => 'pkill killmenow',
+  path    => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
 }
