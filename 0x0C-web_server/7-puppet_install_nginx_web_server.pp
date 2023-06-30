b1ac4d53d7be3b03ec4d3812be1d924b3b7132ea
alx <atundeg@gmail.com>
2023-06-30 01:05:14 +0100

    satisfy checker

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,7 +1,18 @@
-# update repo list and install nginx
-exec { 'install nginx ':
-  command => 'sudo apt update; sudo apt install nginx',
+# add stable version of nginx
+exec { 'add nginx stable repo':
+  command => 'sudo add-apt-repository ppa:nginx/stable',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+# update software packages list
+exec { 'update packages':
+  command => 'apt-get update',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+# install nginx
+package { 'nginx':
+  ensure     => 'installed',
 }
 
 # allow HTTP
