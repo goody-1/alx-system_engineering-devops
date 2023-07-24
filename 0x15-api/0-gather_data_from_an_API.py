4320225e339057cb22613260f23ee97b0cf7a001
alx <atundeg@gmail.com>
2023-07-24 19:51:29 +0100

    collect employee tasks from api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Return Employee task information from REST API"""
+import sys
+
+import requests
+
+if __name__ == "__main__":
+    emp_id = sys.argv[1]
+    emp_url = f"https://jsonplaceholder.typicode.com/users/{emp_id}"
+    todos_url = f"{emp_url}/todos"
+
+    total_tasks = requests.get(todos_url).json()
+    done_tasks = requests.get(f"{todos_url}?completed=true").json()
+    emp_name = requests.get(emp_url).json()["name"]
+
+    text = f"Employee {emp_name} is done with tasks\
+({len(done_tasks)}/{len(total_tasks)}):"
+
+    print(text)
+    for task in done_tasks:
+        print(f"     {task['title']}")
