100279776fad0b3a7a5a248ddd613c1efc84d02f
alx <atundeg@gmail.com>
2023-07-24 19:58:16 +0100

    use get to collect dictionary key

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,11 +11,11 @@ if __name__ == "__main__":
 
     total_tasks = requests.get(todos_url).json()
     done_tasks = requests.get(f"{todos_url}?completed=true").json()
-    emp_name = requests.get(emp_url).json()["name"]
+    emp_name = requests.get(emp_url).json().get("name")
 
     text = f"Employee {emp_name} is done with tasks\
 ({len(done_tasks)}/{len(total_tasks)}):"
 
     print(text)
     for task in done_tasks:
-        print(f"     {task['title']}")
+        print(f"     {task.get('title')}")
